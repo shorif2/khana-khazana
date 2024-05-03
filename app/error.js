5d1e2d5d27db175a1dda9d64ebf2f956590f7368
Shorif <shorifahamed571@gmail.com>
2024-05-03 20:46:21 +0600

    Image Updat2e

--- app/error.js
+++ app/error.js
@@ -9,16 +9,20 @@ export default function Error({ error, reset }) {
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="h-[60vh] flex justify-center items-center">
+      <div className="flex flex-col space-y-4 p-10 border rounded-md shadow-lg justify-center items-center">
+        <h2>Something went wrong! 😢</h2>
+        <h2 className="text-red-500">{error?.toString()}</h2>
+        <button
+          className="py-2 px-4 bg-red-500 rounded-md text-white hover:bg-red-400"
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          Try again
+        </button>
+      </div>
     </div>
   );
 }
