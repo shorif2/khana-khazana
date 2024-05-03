f8664c7b1adca95d80bd06c6563e0f36708cc203
Shorif <shorifahamed571@gmail.com>
2024-05-03 23:24:24 +0600

    Banner Update

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,12 +1,12 @@
 const HeroSection = () => {
   return (
     <section className="container">
-      <div className="py-4 bg-[url('/cover.png')] rounded-lg p-4 md:p-12 min-h-[450px] bg-cover grid place-items-center grid-cols-12">
+      <div className="py-4 bg-[url('/cover.png')] group  rounded-lg p-4 md:p-12 min-h-[450px] bg-cover grid place-items-center grid-cols-12">
         <div className="col-span-12 md:col-span-6">
-          <h1 className="font-bold text-3xl md:text-5xl text-white">
+          <h1 className="font-bold text-3xl md:text-5xl group-hover:text-gray-400 text-white">
             Choose from thousands of recipes
           </h1>
-          <p className="text-white my-4">
+          <p className="text-white my-4 group-hover:text-gray-400">
             Appropriately integrate technically sound value with scalable
             infomediaries negotiate sustainable strategic theme areas
           </p>
